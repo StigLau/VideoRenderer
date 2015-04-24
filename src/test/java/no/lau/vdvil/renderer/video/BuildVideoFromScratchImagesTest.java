16108540c8e86d1c7f21f41fc1d1233f11506861
stig.lau <stig@lau.no>
2015-04-24 16:56:53 +0200

    Second Building video from scratch works #15

--- src/test/java/no/lau/vdvil/renderer/video/BuildVideoFromScratchImagesTest.java
+++ src/test/java/no/lau/vdvil/renderer/video/BuildVideoFromScratchImagesTest.java
@@ -4,6 +4,9 @@ import no.lau.vdvil.domain.MediaFile;
 import no.lau.vdvil.domain.VideoStillImageSegment;
 import no.lau.vdvil.domain.out.Komposition;
 import no.lau.vdvil.renderer.video.creator.ImageBufferStore;
+import no.lau.vdvil.renderer.video.creator.filter.PercentageSplitter;
+import no.lau.vdvil.renderer.video.creator.filter.Reverter;
+import no.lau.vdvil.renderer.video.creator.filter.TaktSplitter;
 import no.lau.vdvil.renderer.video.stigs.ImageSampleInstruction;
 import no.lau.vdvil.renderer.video.stigs.TimeStampFixedImageSampleSegment;
 import org.apache.commons.codec.digest.DigestUtils;
@@ -24,8 +27,9 @@ public class BuildVideoFromScratchImagesTest {
 
     String sobotaMp3 = "/Users/stiglau/vids/The_Hurt_feat__Sam_Mollison_Andre_Sobota_Remix.mp3";
 
-    private String result = "file:///tmp/from_scratch_images_test.mp4";
+    private String result1 = "file:///tmp/from_scratch_images_test_1.mp4";
     private String result2 = "file:///tmp/from_scratch_images_test_2.mp4";
+    private String result3 = "file:///tmp/from_scratch_images_test_3.mp4";
 
 
     @Test
@@ -49,7 +53,7 @@ public class BuildVideoFromScratchImagesTest {
                 new VideoStillImageSegment("First capture sequence", 64, 30)
                 );
 
-        MediaFile mf = new MediaFile(new URL(result), 0f, 128f, "dunno yet");
+        MediaFile mf = new MediaFile(new URL(result1), 0f, 128f, "dunno yet");
         buildKomposition.storageLocation = mf;
 
         CreateVideoFromScratchImages.createVideo(buildKomposition, sobotaMp3, imageStore);
@@ -123,7 +127,7 @@ public class BuildVideoFromScratchImagesTest {
         buildKomposition.framerate = DEFAULT_TIME_UNIT.convert(15, MILLISECONDS);
         buildKomposition.width = 320;
         buildKomposition.height = 200;
-        MediaFile mf = new MediaFile(new URL(result2), 0f, 128f, "cf421566614f958459eb6da008057c83");
+        MediaFile mf = new MediaFile(new URL(result2), 0f, 128f, "c24f323e8ef4588ee30c78dccb9b3472");
         buildKomposition.storageLocation = mf;
 
         CreateVideoFromScratchImages.createVideo(buildKomposition, sobotaMp3, imageStore);
@@ -132,5 +136,55 @@ public class BuildVideoFromScratchImagesTest {
 
     public String md5Checksum(URL url) throws IOException {
         return DigestUtils.md5Hex(url.openStream());
+    }
+
+    @Test
+    public void specificVideoCompositionTest() throws IOException {
+        Komposition fetchKomposition = new Komposition(128,
+                new TimeStampFixedImageSampleSegment("Purple Mountains Clouds", 7541667, 21125000, 8),
+                new TimeStampFixedImageSampleSegment("Besseggen", 21250000, 27625000, 2),
+                new TimeStampFixedImageSampleSegment("Norway showing", 30166667, 34541667, 4),
+                new TimeStampFixedImageSampleSegment("Flower fjord", 35916667, 47000000, 8),
+                new TimeStampFixedImageSampleSegment("Slide Blue mountain top lake", 47083333, 58416667, 8),
+                new TimeStampFixedImageSampleSegment("Fjord foss", 58541667, 64041667, 8),
+                new TimeStampFixedImageSampleSegment("Fjord like river", 64250000, 68958333, 8),
+                new TimeStampFixedImageSampleSegment("Dark lake", 69375000, 74583333, 8),
+                new TimeStampFixedImageSampleSegment("Mountain range", 74833333, 80250000, 8),
+                new TimeStampFixedImageSampleSegment("Omnious fjord Lightbrake", 80291667, 88666667, 8),
+                new TimeStampFixedImageSampleSegment("Boat village panorama", 88750000, 97000000, 8),
+                new TimeStampFixedImageSampleSegment("Village street", 97083333, 102541667, 8),
+                new TimeStampFixedImageSampleSegment("Seaside houses Panorama", 102583333, 108791667, 8),
+                new TimeStampFixedImageSampleSegment("Bergen movement", 108916667, 113541667, 8)
+        );
+        ImageBufferStore imageStore = new ImageBufferStore();
+
+        new VideoThumbnailsCollector(imageStore).capture(downmixedOriginalVideo, fetchKomposition);
+
+
+        int bpm = 124;
+        Komposition buildKomposition =  new Komposition(bpm,
+                /*
+                new VideoStillImageSegment("Purple Mountains Clouds", 0, 16).filter(new TaktSplitter(1)),
+                new VideoStillImageSegment("Purple Mountains Clouds", 16, 16).filter(new TaktSplitter(2)),
+                new VideoStillImageSegment("Purple Mountains Clouds", 32, 16).filter(new TaktSplitter(4)),
+                new VideoStillImageSegment("Purple Mountains Clouds", 48, 16).filter(new TaktSplitter(1))
+                */
+                new VideoStillImageSegment("Dark lake", 0, 4).filter(new TaktSplitter(1)),
+                new VideoStillImageSegment("Purple Mountains Clouds", 4, 4)
+                        .filter(new PercentageSplitter(0, 0.5), new TaktSplitter(1)),
+                new VideoStillImageSegment("Dark lake", 8, 4)
+                        .filter(new TaktSplitter(1), new Reverter()),
+                new VideoStillImageSegment("Purple Mountains Clouds", 12, 4)
+                        .filter(new PercentageSplitter(0.5, 1), new Reverter(), new TaktSplitter(2)),
+                new VideoStillImageSegment("Dark lake", 16, 8)
+        );
+        buildKomposition.framerate = DEFAULT_TIME_UNIT.convert(15, MILLISECONDS);
+        buildKomposition.width = 320;
+        buildKomposition.height = 200;
+        MediaFile mf = new MediaFile(new URL(result3), 0f, 128f, "9bf2c55d6ef8bc7c384ba21f2920e9d1");
+        buildKomposition.storageLocation = mf;
+
+        CreateVideoFromScratchImages.createVideo(buildKomposition, sobotaMp3, imageStore);
+        assertEquals(mf.checksum, md5Checksum(mf.fileName));
     }
 }
