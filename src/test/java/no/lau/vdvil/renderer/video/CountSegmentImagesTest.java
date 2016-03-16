5eb0c50e5a362b555801618d2c376e4d89c8c828
Stig Lau <stig.lau@avella.no>
2016-03-16 16:36:27 +0100

    Added test to see how counting frames from snippets improves the result- #34

--- src/test/java/no/lau/vdvil/renderer/video/CountSegmentImagesTest.java
+++ src/test/java/no/lau/vdvil/renderer/video/CountSegmentImagesTest.java
@@ -25,5 +25,32 @@ public class CountSegmentImagesTest {
         }
         assertEquals(7974, collector.imagesCollected());
     }
+
+    @Test
+    public void countDarkLakeForYourself() throws MalformedURLException {
+        String testVideo = "/tmp/kompost/NORWAY-A_Time-Lapse_Adventure/NORWAY-A_Time-Lapse_Adventure.mp4";
+        VideoInfo.printProperties(VideoInfo.getVideoProperties(testVideo));
+        CountNumberOfEligableImagesBetweenTimestampsCollector collector = new CountNumberOfEligableImagesBetweenTimestampsCollector(69375000, 74000000, testVideo);
+        try {
+            collector.runSingle();
+        }catch (VideoExtractionFinished e) {
+        }
+        //This is the amount of images that were collected using the same parameters as the snippet collector method
+        //Use SnipTest.testSegmentStrip to create the snippet!
+        assertEquals(113, collector.imagesCollected());
+    }
+
+    @Test
+    public void countDarkLakeSnippet() throws MalformedURLException {
+        String testVideo = "/tmp/kompost/NORWAY-A_Time-Lapse_Adventure/NORWAY-A_Time-Lapse_Adventure_Dark_lake_69375000___4625000.mp4";
+        VideoInfo.printProperties(VideoInfo.getVideoProperties(testVideo));
+        CountNumberOfEligableImagesBetweenTimestampsCollector collector = new CountNumberOfEligableImagesBetweenTimestampsCollector(0, VideoInfo.getVideoProperties(testVideo).getDuration(), testVideo);
+        try {
+            collector.runSingle();
+        }catch (VideoExtractionFinished e) {
+        }
+        //This is the amount of images actually extractable from the snippet!!!! Compare 101 to the original expectation 113!
+        assertEquals(101, collector.imagesCollected());
+    }
 }
 
