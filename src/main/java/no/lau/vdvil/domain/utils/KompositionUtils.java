02e373c33268d69f53ab4c41b391d362b5529841
Stig Lau <stig.lau@avella.no>
2016-09-05 12:01:31 +0200

    Moved planner printing to Kompositionsutil

--- src/main/java/no/lau/vdvil/domain/utils/KompositionUtils.java
+++ src/main/java/no/lau/vdvil/domain/utils/KompositionUtils.java
@@ -1,13 +1,20 @@
 package no.lau.vdvil.domain.utils;
 
+import no.lau.vdvil.collector.FrameRepresentation;
+import no.lau.vdvil.collector.KompositionPlanner;
 import no.lau.vdvil.domain.Segment;
 import no.lau.vdvil.domain.out.Komposition;
+import no.lau.vdvil.plan.SuperPlan;
 import no.lau.vdvil.renderer.video.creator.ImageFileStore;
 import no.lau.vdvil.renderer.video.creator.ImageStore;
 import no.lau.vdvil.renderer.video.stigs.ImageSampleInstruction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,6 +22,8 @@ import java.util.stream.Stream;
  * @author Stig@Lau.no 08/04/15.
  */
 public class KompositionUtils {
+    public static Logger logger = LoggerFactory.getLogger(KompositionUtils.class);
+
     public static long fromMillis(Segment segment, Komposition komposition) {
         return calc(segment.start(), komposition.bpm);
     }
@@ -86,5 +95,24 @@ public class KompositionUtils {
 
     public static long findTimeStamp(int frame, int frameRate, Komposition komposition) {
         return calc(frame, komposition.bpm * frameRate);
+    }
+
+    public static void printImageRepresentationImages(KompositionPlanner planner) {
+        Map<Long, List<FrameRepresentation>> reps = new TreeMap<>();
+        for (FrameRepresentation rep : ((SuperPlan) planner.buildPlan()).getFrameRepresentations()) {
+            if(reps.containsKey(rep.timestamp)) {
+                reps.get(rep.timestamp).add(rep);
+            } else {
+                List<FrameRepresentation> gots  = new ArrayList<>();
+                gots.add(rep);
+                reps.put(rep.timestamp, gots);
+            }
+        }
+        for (Long tstamp : reps.keySet()) {
+            logger.info(tstamp + " \t" + reps.get(tstamp).size());
+            if(reps.get(tstamp).size() > 1) {
+                logger.info("Crossfading");
+            }
+        }
     }
 }
