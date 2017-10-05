3ef003c19ff93b1ef4adf9ae937b5c48889397d1
n636552 <stig.lau@nrk.no>
2017-10-05 11:05:13 +0200

    Code goldplating

--- src/main/java/no/lau/vdvil/renderer/video/MediaType.java
+++ src/main/java/no/lau/vdvil/renderer/video/MediaType.java
@@ -3,5 +3,15 @@ package no.lau.vdvil.renderer.video;
 public enum MediaType {
     video,
     audio,
-    unknown
+    unknown;
+
+    public static MediaType typeFinder(ExtensionType extensionType) {
+        if(extensionType.isAudio()) {
+           return MediaType.audio;
+        } else if(extensionType.isVideo()) {
+            return MediaType.video;
+        } else {
+            return MediaType.unknown;
+        }
+    }
 }
