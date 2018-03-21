3db3daf9b44a3dc1f1e7208b409fb301e91b3b48
n636552 <stig.lau@nrk.no>
2018-03-21 10:34:22 +0100

    Fixing tests

--- src/test/java/no/lau/vdvil/renderer/video/VideoInfoTest.java
+++ src/test/java/no/lau/vdvil/renderer/video/VideoInfoTest.java
@@ -5,18 +5,21 @@ import com.xuggle.xuggler.IStreamCoder;
 import org.junit.Test;
 
 
+import java.io.IOException;
+
+import static no.lau.vdvil.domain.utils.KompositionUtils.fetchRemoteFile;
 import static org.junit.Assert.assertEquals;
 
 /**
  * @author Stig Lau 24/03/15.
  */
 public class VideoInfoTest {
-    String filename = "/tmp/kompost/NORWAY-A_Time-Lapse_Adventure/NORWAY-A_Time-Lapse_Adventure.mp4";
+    String norwayRemoteUrl = "https://s3.amazonaws.com/dvl-test-music/test-data/NORWAY-A_Time-Lapse_Adventure/NORWAY-A_Time-Lapse_Adventure.mp4";
 
     @Test
-    public void findOutShit() {
+    public void findOutShit() throws IOException {
         VideoInfo videoInfo = new VideoInfo();
-        IContainer container = videoInfo.getVideoProperties(filename);
+        IContainer container = videoInfo.getVideoProperties(fetchRemoteFile("/tmp/komposttest/", norwayRemoteUrl).toAbsolutePath());
 
         IStreamCoder asd = container.getStream(0).getStreamCoder();
         assertEquals(1280, asd.getWidth());
@@ -24,9 +27,9 @@ public class VideoInfoTest {
     }
 
     @Test
-    public void printProperties() {
+    public void printProperties() throws IOException {
         VideoInfo videoInfo = new VideoInfo();
-        IContainer container = videoInfo.getVideoProperties(filename);
+        IContainer container = videoInfo.getVideoProperties(fetchRemoteFile("/tmp/komposttest/", norwayRemoteUrl).toAbsolutePath());
         videoInfo.printProperties(container);
     }
 }
