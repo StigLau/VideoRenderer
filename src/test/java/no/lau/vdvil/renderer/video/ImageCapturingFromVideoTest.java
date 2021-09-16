b85913c4c40d8bf2fe49819fa881cb6b74a07540
stiglau <{ID}+{username}@users.noreply.github.com>
2021-09-16 21:44:49 +0200

    Test functional

--- src/test/java/no/lau/vdvil/renderer/video/ImageCapturingFromVideoTest.java
+++ src/test/java/no/lau/vdvil/renderer/video/ImageCapturingFromVideoTest.java
@@ -19,21 +19,20 @@ public class ImageCapturingFromVideoTest {
     private Logger logger = LoggerFactory.getLogger(ImageCapturingFromVideoTest.class);
 
     @Test
-    @Ignore //Download first
     public void testCapturing() {
-        String inputFilename = "/tmp/streamingImagesStrippedResult.mp4";
+        String inputFilename = "/tmp/from_scratch_images_test_3c.mp4";
         String outputFilePrefix = "/tmp/snaps/1280_NORWAY-A_Time-Lapse_Adventure.mp4/";
         //String inputFilename = "/tmp/320_NORWAY-A_Time-Lapse_Adventure.mp4";
         //String outputFilePrefix = "/tmp/snaps/320_NORWAY-A_Time-Lapse_Adventure.mp4/";
         //String inputFilename = "/tmp/NORWAY-A_Time-Lapse_Adventure.mp4";
         //String outputFilePrefix = "/tmp/snaps/NORWAY-A_Time-Lapse_Adventure.mp4_2/";
         Komposition komposition = new Komposition(128,
-                new ImageSampleInstruction("First capture sequence", 0, 128, 15)
+                new ImageSampleInstruction("First capture sequence", 0, 48, 15)
                 //new ImageSampleInstruction("Second capture sequence", 300, 16, 1)
         );
         ImageStore ibs = new ImageFileStore<>(komposition, outputFilePrefix);
         new VideoThumbnailsCollector(ibs).capture(inputFilename, komposition);
-        assertEquals(44, ibs.findImagesBySegmentId("First capture sequence 48 + 4").size());
+        assertEquals(310, ibs.findImagesBySegmentId("First capture sequence 0 + 48").size());
 
         logger.info("Found files:");
         for (Segment instruction : komposition.segments) {
